e2901dd3d299f27edba2849feefd24070ad5129c
krishna2906 <krishnapratap2906@gmail.com>
2022-12-14 16:00:05 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         RWOnly obj = new RWOnly();
+        //obj.name = "Krishna";
+        //'name' has private access in 'com.driver.RWOnly'
 
-        obj.name = "Krishna";
         obj.setName("Singh");
         System.out.println(obj.getName());
     }
