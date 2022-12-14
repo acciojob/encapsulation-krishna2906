2881390b92840e58d3703afb2b14da87335b4549
krishna2906 <krishnapratap2906@gmail.com>
2022-12-14 15:43:13 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+
+        obj.name = "Krishna";
+        obj.setName("Singh");
+        System.out.println(obj.getName());
+    }
 }
